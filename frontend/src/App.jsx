1e70238a3c42b5747429c6251560447d86983cc4
LAPTOP-F86FJTEK\82106 <ysh031119@gmail.com>
2025-10-10 15:17:58 +0900

    로그인 완성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,27 @@ import Login from "./component/Login";
 import "./App.css";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const handleLogin = () => {
-        setIsLoggedIn(true);
-    };
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
 
-    let content;
-        if (isLoggedIn) {
-            content = ( 
-                <> <div className="main-content"> <Chat /> <Emotion /> </div> 
-                <Sidebar /></>);
-        } 
-        else {
-            content = <Login onLogin={handleLogin} />;
-        }
-
-    return <div className="app-container">{content}</div>;
-    }
+  return (
+    <div className={`app-container ${isLoggedIn ? "main" : "login"}`}>
+      {isLoggedIn ? (
+        <>
+          <div className="main-content">
+            <Chat />
+            <Emotion />
+          </div>
+          <Sidebar />
+        </>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
+    </div>
+  );
+}
 
 export default App;
