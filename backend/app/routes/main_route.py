bf9c80a704d622452a3f89702543c009ff4fd1ba
박선주 <108116424+sun-risin@users.noreply.github.com>
2025-10-16 05:35:16 +0900

    Revert "Revert "Revert 5 fe""

--- backend/app/routes/main_route.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-main_blp = Blueprint("main", __name__)
-
-@main_blp.route("/")
-def home():
-    return render_template("test.html")
--- backend/app/schemas/user_schema.py
+++
@@ -1,6 +0,0 @@
-from marshmallow import Schema, fields, validate
-
-class UserSchema(Schema):
-    userId = fields.String(required=True)
-    password = fields.String(required=True, validate=validate.Length(min=6))
-    nickname = fields.String(required=True, validate=validate.Length(min=1))
