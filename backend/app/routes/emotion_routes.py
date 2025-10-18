588daf1ba9976c92068d357778765626649a9e51
sunju1 <ciwrssn10@naver.com>
2025-10-19 04:16:45 +0900

    감정선택 구현 중 - 감정 이름과 문서 ID 매핑

--- backend/app/routes/emotion_routes.py
+++ backend/app/routes/emotion_routes.py
@@ -3,3 +3,24 @@ from firebase_admin import firestore
 
 emotion_blp = Blueprint("emotion", __name__, url_prefix="/api/emotion")
 db = firestore.client()
+
+emotion = { # firestore에서 emotionName으로 매핑될 문서 ID
+    "행복" : "happiness",
+    "신남": "excited",
+    "화남" : "aggro",
+    "슬픔" : "sorrow",
+    "긴장" : "nervous" }
+
+# 감정 선택, 
+@emotion_blp.route("/select", methods=["POST"])
+def emotion_select():
+    data = request.get_json()
+    emotionName = data["emotionName"]
+    try:
+        docId = emotion[emotionName]
+    except: # 매핑되는 감정값이 없을 때
+        return jsonify({"message": "잘못된 감정값"}), 400 
+    
+    
+    emotion_doc = db.collection("emotionCategory").document(docId).get()
+    
