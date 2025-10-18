2a52ea3ed4a119db70121d262ac13e0b0e9399d6
sunju1 <ciwrssn10@naver.com>
2025-10-19 03:55:58 +0900

    emotion schema 업뎃 (내용 간략)

--- backend/app/schemas/emotion_schema.py
+++ backend/app/schemas/emotion_schema.py
@@ -1,6 +1,8 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import Schema, fields
 
 class EmotionCategorySchema(Schema):
     emotionName = fields.String()
-    empathyMent = fields.String()
-    trackTraits = fields.String()
+    empathyMent = fields.List()
+    trackTraits = fields.Mapping()
+    
+# 따로 코드로 추가하거나 할 일은 없기 때문에 일단은 이렇게만 작성
