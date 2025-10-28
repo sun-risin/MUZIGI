1fc5c44ae536c43a2bd1f8b6f4f542f9366159fe
sunju1 <ciwrssn10@naver.com>
2025-10-29 02:19:20 +0900

    quata exceed 에러 풀리면 다시 실행할 거임...ㅜㅜ

--- backend/app/schemas/track_schema.py
+++ backend/app/schemas/track_schema.py
@@ -19,68 +19,96 @@ db = firestore.client()
 Track_CSV_PATH = "C:/Users/user/spotify_songs.csv"
 Track_df = pd.read_csv(Track_CSV_PATH)
 
-Track_df = Track_df[['track_id', 'track_name', 'track_artist', 'danceability', 'energy', 'valence']]
+Track_df = Track_df[['track_id', 'track_name', 'track_artist', 
+                     'danceability', 'energy', 'valence', 'track_popularity']]
 
-# 중복, NULL 값 처리
-Track_df = Track_df.drop_duplicates()
-Track_df = Track_df.dropna()
+# 중복, NULL 값 처리, 인덱스 재정립
+Track_df = Track_df.drop_duplicates().dropna().reset_index(drop=True)
 
-COLLECTION_NAME = "Tracks" # Firestore 컬렉션 이름
+# COLLECTION_NAME = "Tracks" # Firestore 컬렉션 이름
 batch = db.batch()
 count = 0
 
-start_index = 19500  # 어제 저장 중단된 지점
+# 데이터 내 한국 아티스트 리스트
+korean_artists = [
+    "TWICE", "SHINee", "BTS", "f(x)", "BoA", "2NE1", "PSY",
+    "GOT7", "Red Velvet", "BLACKPINK", "GFRIEND", "SEVENTEEN",
+    "NCT 127", "Wanna One", "ITZY", "Stray Kids", "ATEEZ",
+    "DREAMCATCHER", "TAEYEON", "(G)I-DLE", "CHUNG HA",
+    "EVERGLOW", "Rain", "IZ*ONE", "4Minute", "SUNMI", "DEAN",
+    "PENTAGON", "TAEMIN", "HyunA", "WINNER", "EXID", "MOMOLAND",
+    "iKON", "CL", "SuperM", "Ailee"
+]
 
-print(f"'{Track_CSV_PATH}' 파일 읽기 완료. 총 {len(Track_df)}개의 행을 '{COLLECTION_NAME}' 컬렉션에 업로드 시작합니다.")
+# 1차 DataFrame: 한국 아티스트만, 인기 점수 열은 필요 없으니 제거
+Track_df_kpop = Track_df[Track_df['track_artist'].isin(korean_artists)]
+Track_df_kpop = Track_df_kpop.loc[:, Track_df_kpop.columns != 'track_popularity']
+
+# 2차 DataFrame: 한국 아티스트 제외 인기 점수 55점 이상
+Track_df_popular_foreign = Track_df[
+    (~Track_df['track_artist'].isin(korean_artists)) &
+    (Track_df['track_popularity'] >= 55)]
+# 이제 인기 점수 필요 없음
+Track_df_popular_foreign = Track_df_popular_foreign.loc[:, Track_df_popular_foreign.columns != 'track_popularity']
+
+print(f"'{Track_CSV_PATH}' 파일 읽기 완료. Kpop 관련: {len(Track_df_kpop)}, 이외 인기 곡: {len(Track_df_popular_foreign)}")
 
 
-# 19500인덱스부터 나머지 재업로드
-for i in range(start_index, len(Track_df)):
-    row = Track_df.iloc[i]
+# 한국 아티스트 내용 저장 컬렉션 - TracksKpop
+# DataFrame의 각 행 순회 / 'Track_df_kpop.iterrows()': (인덱스, 행(Series)) 튜플 반환
+for index, row in Track_df_kpop.iterrows():
+
+    doc_data = row.to_dict() # csv 행 데이터 => Firestore 문서의 데이터 (딕셔너리 형태로 넣어야 됨)
+
+    # 문서 ID 지정
+    doc_id = str(row['track_id']) # spotify 제공 id
+    doc_ref = db.collection("TracksKpop").document(doc_id)
     
-    doc_data = row.to_dict() 
-    doc_id = str(row['track_id'])
-    doc_ref = db.collection(COLLECTION_NAME).document(doc_id)
-
+    # Firestore 필드에 ID가 중복 저장되지 않게 딕셔너리에서 문서 ID로 썼던 행 지움
     doc_data_without_id = row.drop('track_id').to_dict() 
     batch.set(doc_ref, doc_data_without_id)
     
-    if (i+1) % 500 == 0:
-        print(f"{count}개 행 배치 쓰기...")
+    count += 1
+    
+    # Firestore는 배치(batch) 쓰기 시 500개 제한 有, 500개마다 한 번씩 커밋(전송)!
+    if count % 500 == 0:
+        print(f"{count}개 행 배치 쓰기 중...")
         batch.commit()
-        batch = db.batch()
+        batch = db.batch() # 새 배치 시작
 
-if (i+1) % 500 != 0:
-    print(f"마지막 배치({len(Track_df) - (i//500)*500}개) 쓰기 중...")
+# 남은 데이터가 있다면 마지막으로 커밋
+if count % 500 != 0:
+    print(f"마지막 배치({count % 500}개) 쓰기 중...")
     batch.commit()
 
-print(f"총 {len(Track_df) - start_index}개의 문서 업로드 완료 (인덱스 {start_index}~{len(Track_df)-1})")
+print(f"Kpop 곡들, 총 {count}개의 문서 TracksKpop에 업로드 완료!")
 
+count = 0 # 다시 0개
 
-# # DataFrame의 각 행 순회 / 'Track_df.iterrows()': (인덱스, 행(Series)) 튜플 반환
-# for index, row in Track_df.iterrows():
+# 이외 인기곡 저장 컬렉션 - TracksPopular
+for index, row in Track_df_popular_foreign.iterrows():
 
-#     doc_data = row.to_dict() # csv 행 데이터 => Firestore 문서의 데이터 (딕셔너리 형태로 넣어야 됨)
+    doc_data = row.to_dict() # csv 행 데이터 => Firestore 문서의 데이터 (딕셔너리 형태로 넣어야 됨)
 
-#     # 문서 ID 지정
-#     doc_id = str(row['track_id']) # spotify 제공 id
-#     doc_ref = db.collection(COLLECTION_NAME).document(doc_id)
+    # 문서 ID 지정
+    doc_id = str(row['track_id']) # spotify 제공 id
+    doc_ref = db.collection("TracksPopular").document(doc_id)
     
-#     # Firestore 필드에 ID가 중복 저장되지 않게 딕셔너리에서 문서 ID로 썼던 행 지움
-#     doc_data_without_id = row.drop('track_id').to_dict() 
-#     batch.set(doc_ref, doc_data_without_id)
+    # Firestore 필드에 ID가 중복 저장되지 않게 딕셔너리에서 문서 ID로 썼던 행 지움
+    doc_data_without_id = row.drop('track_id').to_dict() 
+    batch.set(doc_ref, doc_data_without_id)
     
-#     count += 1
+    count += 1
     
-#     # Firestore는 배치(batch) 쓰기 시 500개 제한 有, 500개마다 한 번씩 커밋(전송)!
-#     if count % 500 == 0:
-#         print(f"{count}개 행 배치 쓰기 중...")
-#         batch.commit()
-#         batch = db.batch() # 새 배치 시작
+    # Firestore는 배치(batch) 쓰기 시 500개 제한 有, 500개마다 한 번씩 커밋(전송)!
+    if count % 500 == 0:
+        print(f"{count}개 행 배치 쓰기 중...")
+        batch.commit()
+        batch = db.batch() # 새 배치 시작
 
-# # 남은 데이터가 있다면 마지막으로 커밋
-# if count % 500 != 0:
-#     print(f"마지막 배치({count % 500}개) 쓰기 중...")
-#     batch.commit()
+# 남은 데이터가 있다면 마지막으로 커밋
+if count % 500 != 0:
+    print(f"마지막 배치({count % 500}개) 쓰기 중...")
+    batch.commit()
 
-# print(f"총 {count}개의 문서 업로드 완료!")
+print(f"해외 인기곡들, 총 {count}개의 문서 TracksPopular에 업로드 완료!")
