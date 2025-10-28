5f9ef8284d04068ad7289f3769de028780516f63
sunju1 <ciwrssn10@naver.com>
2025-10-28 21:26:27 +0900

    마저 업로드할 예정

--- backend/app/schemas/track_schema.py
+++ backend/app/schemas/track_schema.py
@@ -29,32 +29,58 @@ COLLECTION_NAME = "Tracks" # Firestore 컬렉션 이름
 batch = db.batch()
 count = 0
 
+start_index = 19500  # 어제 저장 중단된 지점
+
 print(f"'{Track_CSV_PATH}' 파일 읽기 완료. 총 {len(Track_df)}개의 행을 '{COLLECTION_NAME}' 컬렉션에 업로드 시작합니다.")
 
-# DataFrame의 각 행 순회 / 'Track_df.iterrows()': (인덱스, 행(Series)) 튜플 반환
-for index, row in Track_df.iterrows():
 
-    doc_data = row.to_dict() # csv 행 데이터 => Firestore 문서의 데이터 (딕셔너리 형태로 넣어야 됨)
-
-    # 문서 ID 지정
-    doc_id = str(row['track_id']) # spotify 제공 id
-    doc_ref = db.collection(COLLECTION_NAME).document(doc_id)
+# 19500인덱스부터 나머지 재업로드
+for i in range(start_index, len(Track_df)):
+    row = Track_df.iloc[i]
     
-    # Firestore 필드에 ID가 중복 저장되지 않게 딕셔너리에서 문서 ID로 썼던 행 지움
+    doc_data = row.to_dict() 
+    doc_id = str(row['track_id'])
+    doc_ref = db.collection(COLLECTION_NAME).document(doc_id)
+
     doc_data_without_id = row.drop('track_id').to_dict() 
     batch.set(doc_ref, doc_data_without_id)
     
-    count += 1
-    
-    # Firestore는 배치(batch) 쓰기 시 500개 제한 有, 500개마다 한 번씩 커밋(전송)!
-    if count % 500 == 0:
-        print(f"{count}개 행 배치 쓰기 중...")
+    if (i+1) % 500 == 0:
+        print(f"{count}개 행 배치 쓰기...")
         batch.commit()
-        batch = db.batch() # 새 배치 시작
+        batch = db.batch()
 
-# 남은 데이터가 있다면 마지막으로 커밋
-if count % 500 != 0:
-    print(f"마지막 배치({count % 500}개) 쓰기 중...")
+if (i+1) % 500 != 0:
+    print(f"마지막 배치({len(Track_df) - (i//500)*500}개) 쓰기 중...")
     batch.commit()
 
-print(f"총 {count}개의 문서 업로드 완료!")
+print(f"총 {len(Track_df) - start_index}개의 문서 업로드 완료 (인덱스 {start_index}~{len(Track_df)-1})")
+
+
+# # DataFrame의 각 행 순회 / 'Track_df.iterrows()': (인덱스, 행(Series)) 튜플 반환
+# for index, row in Track_df.iterrows():
+
+#     doc_data = row.to_dict() # csv 행 데이터 => Firestore 문서의 데이터 (딕셔너리 형태로 넣어야 됨)
+
+#     # 문서 ID 지정
+#     doc_id = str(row['track_id']) # spotify 제공 id
+#     doc_ref = db.collection(COLLECTION_NAME).document(doc_id)
+    
+#     # Firestore 필드에 ID가 중복 저장되지 않게 딕셔너리에서 문서 ID로 썼던 행 지움
+#     doc_data_without_id = row.drop('track_id').to_dict() 
+#     batch.set(doc_ref, doc_data_without_id)
+    
+#     count += 1
+    
+#     # Firestore는 배치(batch) 쓰기 시 500개 제한 有, 500개마다 한 번씩 커밋(전송)!
+#     if count % 500 == 0:
+#         print(f"{count}개 행 배치 쓰기 중...")
+#         batch.commit()
+#         batch = db.batch() # 새 배치 시작
+
+# # 남은 데이터가 있다면 마지막으로 커밋
+# if count % 500 != 0:
+#     print(f"마지막 배치({count % 500}개) 쓰기 중...")
+#     batch.commit()
+
+# print(f"총 {count}개의 문서 업로드 완료!")
