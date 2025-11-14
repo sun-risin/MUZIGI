65c88fb89b67abec5e29c99de62fbdb9f4d1c793
sunju1 <ciwrssn10@naver.com>
2025-11-14 22:09:31 +0900

    feat: validate 검사 위해 required=True 추가

--- backend/app/schemas/playlist_schema.py
+++ backend/app/schemas/playlist_schema.py
@@ -1,11 +1,11 @@
 from marshmallow import Schema, fields
 
 class PlaylistSchema(Schema):
-    emotionName = fields.String()
-    playlistId = fields.String()
-    userDocId = fields.String()
+    emotionName = fields.String(required=True)
+    playlistId = fields.String(required=True)
+    userDocId = fields.String(required=True)
     
 class PlaylistHistorySchema(Schema):
-    playlistId = fields.String()
-    tracks = fields.List(fields.Mapping(fields.String()))
+    playlistId = fields.String(required=True)
+    tracks = fields.List(fields.Mapping(fields.String()), required=True)
     # tracks -> 딕셔너리 배열. 각 인덱스에 가수, 제목, 음악 id 정보가 있다
