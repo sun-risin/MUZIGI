5b210e65738238ba4076cfd26d9b627610eff1ae
sunju1 <ciwrssn10@naver.com>
2025-11-14 20:43:16 +0900

    style: 재생목록 내역 schmea 설명 주석

--- backend/app/schemas/playlist_schema.py
+++ backend/app/schemas/playlist_schema.py
@@ -8,3 +8,4 @@ class PlaylistSchema(Schema):
 class PlaylistHistorySchema(Schema):
     playlistId = fields.String()
     tracks = fields.List(fields.Mapping(fields.String()))
+    # tracks -> 딕셔너리 배열. 각 인덱스에 가수, 제목, 음악 id 정보가 있다
