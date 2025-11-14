7ed9497484b37b853d3475725f0826b32aa9ac77
sunju1 <ciwrssn10@naver.com>
2025-11-14 20:31:41 +0900

    docs: 재생목록 관련 schema

---
+++ backend/app/schemas/playlist_schema.py
@@ -0,0 +1,15 @@
+from marshmallow import Schema, fields
+
+class PlaylistSchema(Schema):
+    chatId = fields.String()
+    content = fields.String()
+    messageId = fields.String()
+    senderType = fields.Boolean()
+    senderId = fields.String()
+    
+class PlaylistHistorySchema(Schema):
+    chatId = fields.String()
+    content = fields.String()
+    messageId = fields.String()
+    senderType = fields.Boolean()
+    senderId = fields.String()
