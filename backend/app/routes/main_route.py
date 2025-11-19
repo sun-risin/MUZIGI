80d4a5ef08d2c1e187b9ffd99f24edde6c48db27
sunju1 <ciwrssn10@naver.com>
2025-11-19 22:56:59 +0900

    test: 테스트 페이지 경로로 나눔

--- backend/app/routes/main_route.py
+++ backend/app/routes/main_route.py
@@ -5,4 +5,7 @@ main_blp = Blueprint("main", __name__)
 @main_blp.route("/")
 def home():
     return render_template("test.html")
-    # return render_template("test2.html") # spotify 액세스토큰 테스트페이지
+
+@main_blp.route("/spoti")
+def aboutSpotify():
+    return render_template("test2.html")
