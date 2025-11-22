225f44f46c575033407a42e19457438c36a47112
sunju1 <ciwrssn10@naver.com>
2025-11-23 02:51:10 +0900

    style: 안 쓰는 라이브러리 import에서 제거

--- backend/app/routes/playlist_routes.py
+++ backend/app/routes/playlist_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 from firebase_admin import firestore
 from app.routes.auth_routes import login_required
-from app.schemas.playlist_schema import PlaylistSchema, TrackInfoSchema
 import requests
 
 # 서비스 레이어의 함수들 import
@@ -17,9 +16,6 @@ from app.services.playlist_services import (spotify_getCurrentUser,
 
 playlist_blp = Blueprint("playlist", __name__, url_prefix="/api/playlist")
 db = firestore.client()
-playlist_schema = PlaylistSchema()
-trackInfo_schema = TrackInfoSchema()
-
     
 # --- 뮤지기 서비스 API들
 # 재생목록 생성 API
