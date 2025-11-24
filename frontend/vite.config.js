09f5424ab84c6c1daf0467203a9a37244cac375f
LAPTOP-F86FJTEK\82106 <ysh031119@gmail.com>
2025-11-25 02:29:06 +0900

    오류 수정 중

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    build: {
+    outDir: 'dist', 
+  },
+  
   server: {
-    host: '127.0.0.1', // `host` 옵션을 `127.0.0.1`로 설정
+    host: '127.0.0.1',
     hmr: {
-      host: '127.0.0.1', // HMR도 같은 값으로 설정
+      host: '127.0.0.1',
     },
   }
 })
