3192440c9bccdc57e06f6299b2c8ffa132b774f8
sunju1 <ciwrssn10@naver.com>
2025-11-25 01:14:47 +0900

    build:

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,16 +7,36 @@ VENV_DIR="${APP_DIR}/backend/venv"
 echo "[AfterInstall] CodeDeploy가 파일 복사 완료 -> 설치 단계 시작... : to ${APP_DIR}"
 cd "${APP_DIR}"
 
-# GitHub Secrets에 저장된 Firebase Base64 문자열이 존재하면 JSON으로 복원
-if [ -n "$FIREBASE_CREDENTIALS_B64" ]; then
-  echo "[AfterInstall] FIREBASE_CREDENTIALS_B64 값을 JSON으로 디코딩합니다"
-  
-  mkdir -p "${APP_DIR}/firebase"
-  echo "$FIREBASE_CREDENTIALS_B64" | base64 --decode > "${APP_DIR}/firebase/serviceAccountKey.json"
-  
-  # 권한을 600으로 제한 (보안 목적)
-  chmod 600 "${APP_DIR}/firebase/serviceAccountKey.json"
+
+# .env 존재 여부 확인
+if [ ! -f "${APP_DIR}/.env" ]; then
+    echo "[ERROR] .env 파일이 존재하지 않습니다"
+    exit 1
 fi
+
+# .env 로부터 환경변수 로드
+echo "[AfterInstall] .env 환경변수 로드"
+set -o allexport
+source "${APP_DIR}/.env"
+set +o allexport
+
+# FIREBASE_CREDENTIALS_B64 검증
+if [ -z "${FIREBASE_CREDENTIALS_B64}" ]; then
+    echo "[ERROR] FIREBASE_CREDENTIALS_B64 값이 비어 있습니다!"
+    exit 1
+fi
+
+echo "[AfterInstall] FIREBASE_CREDENTIALS_B64 -> JSON 디코딩"
+
+# firebase 폴더 생성
+mkdir -p "${APP_DIR}/firebase"
+# Base64 → JSON 디코딩
+echo "$FIREBASE_CREDENTIALS_B64" | base64 --decode > "${APP_DIR}/firebase/serviceAccountKey.json"
+# 보안 위해 권한 수정
+chmod 600 "${APP_DIR}/firebase/serviceAccountKey.json"
+
+echo "[AfterInstall] Firebase 서비스 키 생성 완료"
+
 
 # create/refresh virtualenv
 echo "[AfterInstall] 새 Python 가상환경 생성..."
