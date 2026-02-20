5b4a2ac197a4fe4851b7dece9dd20c0279529a38
sunju1 <ciwrssn10@naver.com>
2026-02-20 23:59:14 +0900

    docs: 현재 user 스키마 구조에 맞게 수정

--- backend/app/domains/user/user_schema.py
+++ backend/app/domains/user/user_schema.py
@@ -5,6 +5,8 @@ class UserSchema(Schema):
     password = fields.String(required=True, validate=validate.Length(min=6))
     nickname = fields.String(required=True, validate=validate.Length(min=1))
     userDocId = fields.String()
-    chatIds = fields.List(fields.String())
-    playlistIds = fields.List(fields.String())
-    userDocId = fields.String()
+    
+    chatIds = fields.List(fields.String()) # 채팅 아이디들
+    
+    playlistIds = fields.Dict( # 감정별 재생목록 아이디
+        keys=fields.String(), values=fields.String())
