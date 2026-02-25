69a26bd4b12d26dad5d301f7a6be6db611261de1
sunju1 <ciwrssn10@naver.com>
2026-02-25 22:48:28 +0900

    docs: 회원가입 관련 custom errorcode 추가

--- backend/app/common/exception/errorCode.py
+++ backend/app/common/exception/errorCode.py
@@ -3,10 +3,16 @@
 from enum import Enum
 
 class ErrorCode(Enum):
+    # --- 400
+    UNVALID_REGISTER_INFO = ("4_000_UNVALID_REGISTER_INFO", "잘못된 회원가입 입력값", 400)
+    
+    # --- 409
+    DUPLICATE_USER = ("900_DUPLICATE_USER", "동일 아이디 존재", 409)
     
     # --- 500
     UNKNOWN_ERR = ("000_UNKNOWN_ERR", "알 수 없는 서버 내부 에러 발생", 500)
-    
+    FAILED_CREATE_CHAT = ("001_FAILED_CREATE_CHAT", "채팅 생성 중 에러 발생", 500)
+    FAILED_REGISTER_USER = ("002_FAILED_REGISTER_USER", "회원가입 정보 저장 중 에러 발생", 500)
     
     # --- 생성자 메서드
     def __init__(self, code: str, message: str, status: int):
