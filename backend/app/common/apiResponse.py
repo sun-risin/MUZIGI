cae656a973c298675554252ab1e4919c390928ba
sunju1 <ciwrssn10@naver.com>
2026-02-25 20:50:11 +0900

    docs: api 응답 포맷에 맞춰 객체 생성하는 정적 메소드 작성

--- backend/app/common/apiResponse.py
+++ backend/app/common/apiResponse.py
@@ -1,1 +1,32 @@
 # 응답 포맷 관련 파일
+
+from flask import jsonify
+from typing import Any
+
+class ApiResponse:
+    
+    # 성공
+    @staticmethod
+    def success(
+        status: int,
+        message: str = "성공", data: Any = None):
+        
+        return jsonify({
+            "success" : True,
+            "code" : str(status),
+            "message" : message,
+            "data" : data,
+        }), status
+        
+    # 실패
+    @staticmethod
+    def error(
+        status: int,
+        message: str = "실패", code: str = "000_UNKNOWN_ERR"):
+        
+        return jsonify({
+            "success" : False,
+            "code" : code,
+            "message" : message,
+            "data" : None,
+        }), status
