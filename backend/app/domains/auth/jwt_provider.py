e3deb063574a35c5cf673c6bf7be38c127fd0d30
sunju1 <ciwrssn10@naver.com>
2026-02-26 19:53:08 +0900

    refactor: 토큰 payload 추출 로직 분리

--- backend/app/domains/auth/jwt_provider.py
+++ backend/app/domains/auth/jwt_provider.py
@@ -11,3 +11,13 @@ def generate_token(user_docId: str, doc_nickname: str):
                 current_app.config['MUZIGI_JWT_KEY'], algorithm= 'HS256')
     
     return token
+
+# 토큰 까기
+def extract_token(userToken: str):
+    payload = jwt.decode(
+        userToken, 
+        current_app.config['MUZIGI_JWT_KEY'], algorithms=['HS256'])
+    
+    userDocId = payload["userDocId"]
+    
+    return userDocId
