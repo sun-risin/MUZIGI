dcfe2cedc287caf5daf2a4474f24214261d697bb
sunju1 <ciwrssn10@naver.com>
2026-03-03 20:29:01 +0900

    docs: 예외 종류 클래스 추가

--- backend/app/common/exception/customException.py
+++ backend/app/common/exception/customException.py
@@ -4,4 +4,15 @@ from .errorCode import ErrorCode
 
 class CustomException(Exception):
     def __init__(self, error_code: ErrorCode):
+        super().__init__(error_code.message)
         self.error_code = error_code
+        
+class ValidateException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+        
+class UnknownException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
